3894367eadcbfa81e06eed7f4b238c5e917ff901
Mario López González <mariolopezgonzalez2001@gmail.com>
2022-12-16 14:08:00 +0100

    Password Pic URL

--- app/urls.py
+++ app/urls.py
@@ -61,5 +61,6 @@ urlpatterns = [
     path('api/text_size', TextSizeView),
     path('api/student', StudentView),
     path('api/student/<_id>', StudentViewID),
+    path('api/password_pictogram', PasswordPictogramView),
     path('api/image/<_image>', ImageViewID)
 ] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
